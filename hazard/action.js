12979c5471a29aadcda202a23e0c6b5f090c0216
jackbob <jackbob@jackbobs-MacBook-Air.local>
2020-06-17 15:36:49 -0400

    add website stuff

--- hazard/action.js
+++ hazard/action.js
@@ -1,22 +1,26 @@
-const table_container = document.getElementById("game-winner-container");
-const button_container = document.getElementById("play_button");
-const guess_container = document.getElementById("guess_box");
-
-
-add_row = function(parent, ...data){
-
-    let row = document.createElement("tr");
-
-    for(index = 0; index < data.length; index++){
-        let element_to_add = document.createElement("td");
-        element_to_add.appendChild(document.createTextNode(data[index]))
+function createRows() {
+    var table = document.getElementById("game_winner");
+     //control the number of times the game is played
+    trials = document.getElementByID("trials_box").value;
+    trials = Integer.parseInt(trials);
+    let i = 1;
+     while(i<trials){
+      // number of entries to show
+      var row = table.insertRow(i);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+    
+      // variables to return
+      var game = i;
+      //Insert relevant data
+      cell1.innerHTML = game.toString();
+      cell2.innerHTML = "Phillip";
+      cell3.innerHTML = "MEEEE";
+      cell4.innerHTML = "122112";
+      //increment the counter for the number of simulations
+      i = i+1;
+     }
+    
     }
-
-    parent.appendChild(row);
-
-
-}
-
-add_button.onclick = () =>{
-    add_row(table_container,guess_box.value)
-}
