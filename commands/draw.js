36b93002466960f43418acb00db257627d8a08d3
Anis Chihoub <64680724+achihoub2023@users.noreply.github.com>
2021-08-12 01:39:22 -0400

    Create draw.js

---
+++ commands/draw.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name:`${prefix}draw`,
+    description: "Style Transfer",
+    execute(message, args) {
+		message.reply("Use neural style transfer to upload images and make medieval art here: (Coming Soon)")
+	},
+};
